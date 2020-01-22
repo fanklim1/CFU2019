f23f32ef8cbdd1d0b966eeec2c03473989b5bdd3
fanklim1 <55453349+fanklim1@users.noreply.github.com>
2020-01-22 18:36:19 +0300

    Add files via upload

---
+++ Задание 19.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int n;
+	int all_nums[10] = {};
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		all_nums[num] ++;
+	}
+	for (int i = 0; i < 10; i++) 
+		if (all_nums[i])
+			cout << i << ':' << all_nums[i] << endl;
+	return 0;
+}
